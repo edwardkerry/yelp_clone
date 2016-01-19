c1450016536ace53e5995dd5dc771d1398b90632
Adam Lancaster <lancaster28@hotmail.com>
2016-01-19 16:27:42 +0000

    Rails version 1 walkthrough complete

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -16,5 +16,11 @@ describe Restaurant, type: :model  do
     expect(restaurant).not_to be_valid
     expect(restaurant).to have(1).error_on(:name)
   end
+
+  it "is not valid unless it has a unique name" do
+    Restaurant.create(name: "Moe's Tavern")
+    restaurant = Restaurant.new(name: "Moe's Tavern")
+    expect(restaurant).to have(1).error_on(:name)
+  end
 end
 
