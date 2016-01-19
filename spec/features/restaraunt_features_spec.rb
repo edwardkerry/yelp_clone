72fa22abb13d5fe8d85366235acbbf276703fb1d
Adam Lancaster <lancaster28@hotmail.com>
2016-01-19 10:20:57 +0000

    Restaurant database added

--- spec/features/restaraunt_features_spec.rb
+++ spec/features/restaraunt_features_spec.rb
@@ -8,4 +8,17 @@ feature "restaraunts" do
       expect(page).to have_link "Add a restaurant"
     end
   end
+  context "restaurants have been added" do
+    before do
+      Restaurant.create(name: "Hooters")
+    end
+  
+    scenario "display restaurants" do
+      visit "/restaurants"
+      expect(page).to have_content("Hooters")
+      exect(page).not_to have_content("No restaurants yet")
+    end
+
+  end
+
 end
