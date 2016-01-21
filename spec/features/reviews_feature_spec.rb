fcfbc1915602c4387c7df9ff1925cc54d6bf30f9
edwardkerry <edwardkerry@gmail.com>
2016-01-21 18:57:34 +0000

    Adds Feature test for displaying average review rating

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -4,13 +4,23 @@ feature "reviewing" do
   let!(:nandos){Restaurant.create(name: "Nandos")}
 
   scenario "allows users to leave a review" do
-    visit "/restaurants"
-    click_link "Review Nandos"
-    fill_in "Opinion", with: "I don't even like chicken"
-    select "3", from: "Rating"
-    click_button "Leave Review"
-
+    leave_review("I don't even like chicken", 3)
     expect(current_path).to eq "/restaurants"
     expect(page).to have_content "I don't even like chicken"
   end
+
+  scenario "displays an average rating for all reviews" do
+    leave_review("so so", 3)
+    leave_review("Great", 5)
+    expect(page).to have_content('Average rating: 4')
+  end
+
+  def leave_review(opinion, rating)
+    visit "/restaurants"
+    click_link "Review Nandos"
+    fill_in "Opinion", with: opinion
+    select rating, from: "Rating"
+    click_button "Leave Review"
+  end
+
 end
